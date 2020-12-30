6c8dddf4a214b24582757e7b38b77da22f95f91b
山室 友樹 <yuki.sumaho@gmail.com>
2020-12-30 18:29:42 +0900

    GUIアプリの完成

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # ruby_gui_application
 
+## セットアップ
+
 ```
+rbenv install 2.7.2
+rbenv local 2.7.2
+
 gem install gtk3
+
+brew install glade
 ```
 
-# 動作環境
+## 動作環境
 ruby: 2.7.2
--- application.rb
+++ application.rb
@@ -1,5 +1,23 @@
 require 'gtk3'
-window = Gtk::Window.new
-window.signal_connect(:destroy) { Gtk.main_quit }
-window.show
+
+builder = Gtk::Builder.new(file: 'myapp.glade')
+
+win = builder.get_object('win')
+button = builder.get_object('button')
+
+# [✕] が押された時にアプリを終了する
+def on_win_destroy
+  Gtk.main_quit
+end
+
+# ボタンがクリックされた時にターミナルに出力する
+@counter = 0
+def on_button_clicked
+  puts "やあ、君はボタンを#{@counter}回押したね！"
+  @counter += 1
+end
+
+builder.connect_signals { |handler| method(handler) } # handler は String
+
+win.show_all
 Gtk.main
